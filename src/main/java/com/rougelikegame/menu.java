7020a3ef3663804109d66a852b459611f9be33ea
Pectourus <69641403+Pectourus@users.noreply.github.com>
2022-03-04 13:27:40 -0500

    Initial Main Menu testing

--- src/main/java/com/rougelikegame/menu.java
+++ src/main/java/com/rougelikegame/menu.java
@@ -1,15 +1,16 @@
 package com.rougelikegame;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 
@@ -18,43 +19,48 @@ public class menu extends Application {
     public void start(Stage primaryStage) {
         javafx.scene.text.Font.getFamilies();
         javafx.scene.text.Font.getFontNames();
-
         //Title Text
         Text title = new Text();
 
         title.setText("Roguelike Game");
         title.setFont(Font.font ("Impact", 40));
         title.setFill(Color.BLACK);
-        title.setLayoutX(190);
-        title.setLayoutY(50);
+        title.setTextAlignment(TextAlignment.CENTER);
 
-        //exit bar creation
-        Menu exitGame = new Menu("Exit Game");
 
-        //settings bar and menu creation
-        Menu settings = new Menu("Settings");
 
-        MenuItem setItem1 = new MenuItem("Video/Graphics");
-        MenuItem setItem2 = new MenuItem("Audio");
-        MenuItem setItem3 = new MenuItem("Gameplay");
-        MenuItem setItem4 = new MenuItem("Controls");
-        MenuItem setItem5 = new MenuItem("Exit");
-
-        settings.getItems().addAll(setItem1,setItem2,setItem3,setItem4,setItem5);
 
         //placing menu pieces
-        MenuButton menuSetButton = new MenuButton();
-            menuSetButton.setText("Settings");
-            menuSetButton.setTranslateX(280);
-            menuSetButton.setTranslateY(160);
+        Button SetMainButton = new Button();
+            SetMainButton.setText("Settings");
+            SetMainButton.setAlignment(Pos.CENTER);
+            SetMainButton.setTranslateY(240);
 
-        MenuButton menuExitButton = new MenuButton();
-            menuExitButton.setText("Exit Game");
-            menuExitButton.setTranslateX(275);
-            menuExitButton.setTranslateY(200);
+       Button ExitButton = new Button();
+            ExitButton.setText("Exit Game");
+            ExitButton.setAlignment(Pos.CENTER);
+            ExitButton.setTranslateY(280);
 
-        Group root = new Group(menuSetButton, menuExitButton, title);
-        Scene scene = new Scene(root,640,480, Color.BLANCHEDALMOND);
+            ExitButton.setOnAction(actionEvent -> {
+                Platform.exit();
+            });
+
+        Button PlayButton = new Button();
+            PlayButton.setText("Play Game");
+            PlayButton.setAlignment(Pos.CENTER);
+            PlayButton.setTranslateY(200);
+
+        Button ContButton = new Button();
+            ContButton.setText("Continue");
+            ContButton.setAlignment(Pos.CENTER);
+            ContButton.setTranslateY(160);
+
+        HBox root = new HBox();
+
+
+        root.getChildren().addAll(PlayButton, ContButton, SetMainButton, ExitButton, title);
+
+        Scene scene = new Scene(root, 1280, 720);
         primaryStage.setTitle("Menu Test");
         primaryStage.setScene(scene);
         primaryStage.show();
